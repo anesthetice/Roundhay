dc8af283946fa3107555160b313812e906ca0a17
anesthetice <elliot.currat@epfl.ch>
2023-08-06 16:31:10 +0200

    temp

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
+use std::net::SocketAddr;
 use axum::{
     routing::get,
     Router,
     response::Html,
+};
+
+use tokio::{
+    fs, io::AsyncReadExt,
 };
 
 #[tokio::main]
@@ -9,10 +14,29 @@ async fn main() {
     // build our application with a single route
     let app = Router::new()
         .route("/", get(|| async { Html("<head><meta http-equiv=\"refresh\" content=\"0; URL=/home/\"/></head>") }))
-        .route("/home/", get(|| async { "test" }));
+        .route("/home/", get(|| async { Html("<a href='/res/vid.mp4'>download</a>") }))
+        .route("/res/vid.mp4", get(test));
 
-    axum::Server::bind(&"0.0.0.0:1888".parse().unwrap())
+    axum::Server::bind(&SocketAddr::from(([0,0,0,0], 1888)))
         .serve(app.into_make_service())
         .await
         .unwrap();
+}
+
+async fn test() -> Vec<u8> {
+    let mut file = fs::OpenOptions::new().read(true).open("./res/test.mp4").await.unwrap();
+    // convert the `AsyncRead` into a `Stream`
+    let stream = ReaderStream::new(file);
+    // convert the `Stream` into an `axum::body::HttpBody`
+    let body = StreamBody::new(stream);
+
+    let headers = Headers([
+        (header::CONTENT_TYPE, "text/toml; charset=utf-8"),
+        (
+            header::CONTENT_DISPOSITION,
+            "attachment; filename=\"Cargo.toml\"",
+        ),
+    ]);
+
+    Ok((headers, body))
 }
